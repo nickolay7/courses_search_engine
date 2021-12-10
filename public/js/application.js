a3fa7219efa54fa18da0d0df894c3f8f2f1ec4fb
nickolay7 <shkelal50@gmail.com>
2021-12-10 10:26:10 +0300

    fixed

--- public/js/application.js
+++ public/js/application.js
@@ -2,8 +2,8 @@
 // const BASE_URL = `https://api.telegram.org/bot${TOKEN}/`
 // const getMe = `${BASE_URL}getMe`;
 // const sendMessage = `${BASE_URL}sendMessage?chat_id=1447651842&text=Hello Fedya!`;
-// const getUpdates = `${BASE_URL}getUpdates`; // ?offset='update_id' начиная с которого присылать сообщения, остальные телега метит как прочитаные
-//   // limit=2 ограничение количества ответов
+// const getUpdates = `${BASE_URL}getUpdates`; // ?offset='update_id' начиная с которого присылать сообщения, остальные телега метит как прочитанные
+//   // &limit=2 ограничение количества ответов / &parse_mode=html or &parse_mode=markdown format of message
 // fetch(sendMessage)
 //   .then((response) => response.json())
 //   .then((data) => console.dir(data));
